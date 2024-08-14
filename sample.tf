16d4dbb9a8f65f9b47cabb31c3283fa9c502b299
rajeevbh <rajeevbh15290@gmail.com>
2024-08-14 19:02:36 +0530

    Intial commit

---
+++ sample.tf
@@ -0,0 +1,26 @@
+# Configure the Azure provider
+provider "azurerm" {
+  features = {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Create a subnet
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
